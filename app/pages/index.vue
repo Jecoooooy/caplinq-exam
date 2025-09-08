1595a5a58574526bc5c65d959ba03e4740b6aff4
Jecoooooy <bencitojerico@gmail.com>
2025-09-09 04:40:13 +0800

    feat: enhance index page to display suppliers alongside products

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
-        <Button>t asdeasd st</Button>
+    <div class="h-svh grid container mx-auto grid-cols-2 overflow-auto">
         <pre>{{ products }}</pre>
+        <pre>{{ suppliers }}</pre>
     </div>
 </template>
 <script lang="ts" setup>
-const { data: products, pending, error } = await useFetch("/api/products");
+const {
+    data: products,
+    pending: producsPending,
+    error: productsError,
+} = await useFetch("/api/products");
+const {
+    data: suppliers,
+    pending: suppliersPending,
+    error: suppliersError,
+} = await useFetch("/api/suppliers");
 </script>

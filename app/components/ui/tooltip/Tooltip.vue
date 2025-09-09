080c56184e12a25cc0d36456f809e9e4163cb23d
Jecoooooy <bencitojerico@gmail.com>
2025-09-09 21:07:44 +0800

    feat: enhance Tooltip component with dynamic content handling

--- app/components/ui/tooltip/Tooltip.vue
+++ app/components/ui/tooltip/Tooltip.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
-import type { TooltipRootEmits, TooltipRootProps } from "reka-ui"
-import { TooltipRoot, useForwardPropsEmits } from "reka-ui"
+import type { TooltipRootEmits, TooltipRootProps } from "reka-ui";
+import { TooltipRoot, useForwardPropsEmits } from "reka-ui";
 
-const props = defineProps<TooltipRootProps>()
-const emits = defineEmits<TooltipRootEmits>()
+const props = defineProps<
+    TooltipRootProps & {
+        content?: string | Record<string, unknown>;
+        align?: "start" | "center" | "end";
+        side?: "top" | "bottom" | "left" | "right";
+    }
+>();
+const emits = defineEmits<TooltipRootEmits>();
 
-const forwarded = useForwardPropsEmits(props, emits)
+const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
-  <TooltipRoot
-    data-slot="tooltip"
-    v-bind="forwarded"
-  >
-    <slot />
-  </TooltipRoot>
+    <TooltipProvider>
+        <TooltipRoot data-slot="tooltip" v-bind="forwarded">
+            <TooltipTrigger as-child>
+                <slot />
+            </TooltipTrigger>
+            <TooltipContent
+                :align="align"
+                :align-offset="0"
+                :side="side"
+                :side-offset="-4"
+            >
+                <slot name="content">
+                    <div
+                        v-if="typeof content === 'object' && content"
+                        class="scrollbar-hide max-h-96 max-w-md space-y-2 overflow-auto p-1"
+                    >
+                        <TooltipContentObject :object="content" />
+                    </div>
+                    <div v-else class="text-sm">
+                        {{ content }}
+                    </div>
+                </slot>
+            </TooltipContent>
+        </TooltipRoot>
+    </TooltipProvider>
 </template>

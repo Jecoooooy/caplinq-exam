5224d7a23ec96d9c79fc59ebad428db591b2a4c0
Jecoooooy <bencitojerico@gmail.com>
2025-09-09 20:13:01 +0800

    feat: enhance products API to support pagination and type safety

--- server/api/products.get.ts
+++ server/api/products.get.ts
@@ -1,4 +1,11 @@
-export default defineEventHandler(async () => {
-    const data = await useStorage().getItem('data:products.json')
-    return data
-})
+import { H3Event } from "h3";
+import type { PaginatedResponse, Product } from "@/types/product";
+export default defineEventHandler(
+    async (_event: H3Event): Promise<PaginatedResponse<Product>> => {
+        const data =
+            await useStorage().getItem<PaginatedResponse<Product>>(
+                "data:products.json",
+            );
+        return data!;
+    },
+);

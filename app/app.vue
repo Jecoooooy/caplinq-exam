5299db52b5a827520614dd3556afe92ce16ff486
Jecoooooy <bencitojerico@gmail.com>
2025-09-09 22:20:26 +0800

    feat: add head configuration for SEO and responsive design in app.vue

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,16 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
+<script lang="ts" setup>
+useHead({
+    title: "Caplinq",
+    meta: [
+        { name: "description", content: "front-end developer examination" },
+        {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1, maximum-scale=1",
+        },
+    ],
+    bodyAttrs: {},
+});
+</script>

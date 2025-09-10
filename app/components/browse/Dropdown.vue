1d85b9c6a949f4f1ba7e23764194a54d3cf64ad7
Jecoooooy <bencitojerico@gmail.com>
2025-09-10 22:54:17 +0800

    feat: enhance Dropdown component with improved accessibility and search functionality, including keyboard navigation and ripple effect on item selection

--- app/components/browse/Dropdown.vue
+++ app/components/browse/Dropdown.vue
@@ -2,6 +2,7 @@
     <article class="flex-col flex gap-6">
         <div class="px-10 border-b pb-6">
             <Input
+                class="h-10.5"
                 placeholder="Search Supplier"
                 :icon="{ name: 'magnify', position: 'left' }"
             />
@@ -11,13 +12,16 @@
             ref="container"
             class="pl-10 pr-3 w-[calc(100%-16px)] max-h-60 scroll-py-1 overflow-x-hidden"
         >
-            <ul class="p-0 flex flex-col">
+            <ul class="p-0 flex flex-col" ref="list">
                 <template v-for="(item, index) in allSuppliers" :key="item.id">
                     <li
                         :value="item.id"
-                        tabindex="0"
-                        class="hover:bg-primary h-10 hover:text-primary-foreground text-sm px-4 focus:bg-primary/50 flex justify-between items-center"
+                        tabindex="-1"
+                        class="hover:bg-accent relative h-10 hover:text-accent-foreground text-sm px-4 focus:bg-accent/20 flex justify-between items-center"
+                        @keydown="handleKeyDown($event, index)"
+                        @click="expandList($event, index)"
                     >
+                        <AnimationRipple ref="rippleRef" />
                         {{ item.name }}
                         <Icon name="mdi:chevron-right" size="20" />
                     </li>
@@ -28,9 +32,31 @@
     </article>
 </template>
 <script lang="ts" setup>
+import type Ripple from "~/components/ui/animation/Ripple.vue";
+
+const props = defineProps<{ isOpen: boolean }>();
+
 const { suppliers } = useSupplier();
 const allSuppliers = computed(() => suppliers ?? []);
 
 const container: Ref<HTMLElement | null> = ref(null);
 useEnhancedScrollbar(container, "scroll", "y");
+
+const list: Ref<HTMLUListElement | null> = ref(null);
+const { handleKeyDown } = useArrowNavigation(list);
+
+watch(
+    () => props.isOpen,
+    () => {
+        const first = list.value?.querySelector<HTMLLIElement>("li");
+        setTimeout(() => first?.focus(), 100);
+    },
+    { immediate: true },
+);
+
+const rippleRef = ref<InstanceType<typeof Ripple>[]>([]);
+
+function expandList(event: MouseEvent, index: number) {
+    rippleRef.value[index]?.startRipple(event);
+}
 </script>
